9d477ba2d0b2648230d9453c61bba8637569a1a7
liz <lizalfond@gmail.com>
2026-08-12 15:09:43 +0700

    improvements to images

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/results/", "/maintenance"],
+      disallow: ["/api/", "/maintenance"],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
     host: BASE_URL,
